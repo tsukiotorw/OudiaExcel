62ff74851006d8b31f81d2624853a11e98965801
tsukiotorw <tsukiotorw@gmail.com>
2026-08-30 17:50:08 +0900

    test verify operations in real oud2 data stoptimes Down train 1

--- tests/parser/test_parser_integration.py
+++ tests/parser/test_parser_integration.py
@@ -430,3 +430,60 @@ def test_parse_real_oud2_file() -> None:
         assert stop_time.is_pass == expected_is_pass
         assert stop_time.track_index == expected_track_index
 
+    # StopTimes: DOWN train 1
+    stop_times = railway.diagrams[0].trains[1].stop_times
+
+    assert len(stop_times) == 4
+
+    expected_stop_times = [
+        (0, 0, "A", None, "000", False, 0),
+        (1, 1, "B", "005", "006", False, 0),
+        (2, 2, "C", "009", "015", False, 0),
+        (3, 3, "D", "025", None, False, 0),
+    ]
+
+    for stop_time, (
+        expected_order,
+        expected_station_index,
+        expected_station_name,
+        expected_arrival,
+        expected_departure,
+        expected_is_pass,
+        expected_track_index,
+    ) in zip(stop_times, expected_stop_times):
+        assert stop_time.order == expected_order
+        assert stop_time.station.index == expected_station_index
+        assert stop_time.station.name == expected_station_name
+        assert stop_time.arrival_time == expected_arrival
+        assert stop_time.departure_time == expected_departure
+        assert stop_time.is_pass == expected_is_pass
+        assert stop_time.track_index == expected_track_index
+
+    # StopTimes: UP train 0 pass stations
+    stop_times = railway.diagrams[1].trains[0].stop_times
+
+    assert len(stop_times) == 4
+
+    # B駅
+    stop_time = stop_times[1]
+
+    assert stop_time.order == 1
+    assert stop_time.station.index == 1
+    assert stop_time.station.name == "B"
+    assert stop_time.arrival_time is None
+    assert stop_time.departure_time is None
+    assert stop_time.is_pass is True
+    assert stop_time.track_index == 1
+
+    # C駅
+    stop_time = stop_times[2]
+
+    assert stop_time.order == 2
+    assert stop_time.station.index == 2
+    assert stop_time.station.name == "C"
+    assert stop_time.arrival_time is None
+    assert stop_time.departure_time is None
+    assert stop_time.is_pass is True
+    assert stop_time.track_index == 1
+
+
